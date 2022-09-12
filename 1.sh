dd41ff6255ed95527adff71bbb2335278ce85d5b
xiaozhi_heng <2895695886@qq.com>
2022-09-12 16:59:33 +0800

    commit

---
+++ 1.sh
@@ -0,0 +1,7 @@
+#echo "# heng-star.github.io" >> README.md
+echo "Hello World" > index.html
+git add *
+git commit -m "commit"
+#git branch -M master
+#git remote add origin git@github.com:heng-star/heng-star.github.io.git
+git push -u origin master
